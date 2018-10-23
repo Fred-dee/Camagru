7a4ba8914333e1b58b26eccc07434dd2956e7ac6
Fred-Dee <fred.dilapisho@gmail.com>
2018-10-23 11:32:27 +0200

    The overlay is working and it draws

--- capture.php
+++ capture.php
@@ -13,16 +13,44 @@ header('Content-Type: text/html');
         <?php require_once './includes/main-includes.php'; ?>
         <script src="./js/capture.js" type="text/javascript"></script>
         <style>
-            #container {
+            .container {
+                position: relative;
                 margin: 0px auto;
                 width: 500px;
                 height: 375px;
                 border: 10px #333 solid;
             }
+            
+            .container:hover .overlay
+            {
+                opacity: 1;
+            }
+            .overlay {
+              position: absolute;
+              top: 0;
+              bottom: 0;
+              left: 0;
+              right: 0;
+              height: 100%;
+              width: 100%;
+              opacity: 0;
+              transition: .3s ease;
+              
+            }
             #videoElement {
                 width: 500px;
                 height: 375px;
                 background-color: #666;
+            }
+            .icon {
+              color: white;
+              font-size: 100px;
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              -ms-transform: translate(-50%, -50%);
+              text-align: center;
             }
         </style>
     </head>
@@ -30,11 +58,14 @@ header('Content-Type: text/html');
         <?php
         require_once './includes/navbar.php';
         ?>
-        <div id="container">
+        <div class="container">
             <video autoplay="true" id="videoElement">
             </video>
+            <div class="overlay">
+                <img id="img_overlay" src="./imgs/overlay2.png" alt="" class="img-responsive icon"/>
+            </div>
         </div>
-        <button class="btn btn-primary grey darken-4" onclick="getMedia()" id="btn_snap">Snap</button>
+        <button class="btn btn-primary grey darken-4" onclick="" id="btn_snap">Snap</button>
         <br/>
 
         <canvas id="c" style="display:box;" width="500px" height="375px"></canvas>
@@ -48,14 +79,17 @@ header('Content-Type: text/html');
                 navigator.mediaDevices.getUserMedia({video: true})
                         .then(function (stream) {
                             video.srcObject = stream;
-                    
+                            var over;
+                            over = document.getElementById("img_overlay");
                         var canvas = document.getElementById("c");
                         var button = document.getElementById("btn_snap");
-                        //video.src = stream;
+
                         button.disabled = false;
                         button.onclick = function() {
                             canvas.getContext("2d").drawImage(video, 0, 0, 500, 375, 0, 0, 500, 375);
+                            canvas.getContext("2d").drawImage(over, 0, 0, 500, 375, 0, 0, 500, 375);
                             var img = canvas.toDataURL("image/png");
+                            
                         };
                         })
                         .catch(function (err0r) {
--- js/capture.js
+++ js/capture.js
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
 function hasGetUserMedia() {
     return !!(navigator.mediaDevices &&
             navigator.mediaDevices.getUserMedia);
@@ -22,7 +23,7 @@ function getMedia()
         alert('getUserMedia() is not supported by your browser');
     }
 }
-
+*/
 
 
 
