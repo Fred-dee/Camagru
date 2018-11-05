a0392ed1eaf9ad697200ee21c24e1bf0583e2ac2
Fred-Dee <fred.dilapisho@gmail.com>
2018-11-05 14:52:58 +0200

    Working on errors and stuff

--- private/login_check.php
+++ private/login_check.php
@@ -24,41 +24,51 @@
             if(!$uppercase || !$lowercase || !$number || strlen($password) < 8) {
               login_error(1, "Password should contain at least one upper case, one lowercase one digit and a special character. Password must be of length 8 and above");
             }
-			$stmt = $pdo->prepare("SELECT * FROM users WHERE user_name= ?");
-			$stmt->execute([$username]);
-			$numrows= $stmt->rowCount();
-			if ($numrows != 0)
-			{
-				login_error(1, "Username already exists");
-			}
-			if (cpass != password)
-			{
-				login_error(1, "Passwords do not match");
-			}
-			$hashed = password_hash($password, PASSWORD_DEFAULT);
-			$stmt = $pdo->prepare("INSERT INTO users (`user_name`, `first_name`, `last_name`, `email`, `hash`) VALUES
-			(:uname, :fname, :lname, :email, :hash)
-			");
-			$stmt->bindParam(':uname', $username, PDO::PARAM_STR, 15);
-			$stmt->bindParam(':fname', $fname, PDO::PARAM_STR, 25);
-			$stmt->bindParam(':lname', $lname, PDO::PARAM_STR, 25);
-			$stmt->bindParam(':email', $email, PDO::PARAM_STR);
-			$stmt->bindParam(':hash', $hashed, PDO::PARAM_STR);
-			try
-			{
-				$stmt->execute();
-			}
-			catch (PDOException $e)
-			{
-				login_error(1, $e->getMessage());
-			}
-			$_SESSION["login"] = $username;
-            $stmt = $pdo->prepare("SELECT id FROM users WHERE user_name=:uname");
-            $stmt->bindParam(':uname', $username, PDO::PARAM_STR, 15);
-            $stmt->execute();
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            $_SESSION["user_id"] = (int) $row["id"];
-			header("location: ../index");
+            else
+            {
+                $stmt = $pdo->prepare("SELECT * FROM users WHERE user_name= ?");
+                $stmt->execute([$username]);
+                $numrows= $stmt->rowCount();
+                if ($numrows != 0)
+                {
+                    login_error(1, "Username already exists");
+                }
+                else
+                {
+                    if (cpass != password)
+                    {
+                        login_error(1, "Passwords do not match");
+                    }
+                    else
+                    {
+                        $hashed = password_hash($password, PASSWORD_DEFAULT);
+                        $stmt = $pdo->prepare("INSERT INTO users (`user_name`, `first_name`, `last_name`, `email`, `hash`) VALUES
+                        (:uname, :fname, :lname, :email, :hash)
+                        ");
+                        $stmt->bindParam(':uname', $username, PDO::PARAM_STR, 15);
+                        $stmt->bindParam(':fname', $fname, PDO::PARAM_STR, 25);
+                        $stmt->bindParam(':lname', $lname, PDO::PARAM_STR, 25);
+                        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+                        $stmt->bindParam(':hash', $hashed, PDO::PARAM_STR);
+                        try
+                        {
+                            $stmt->execute();
+                            $_SESSION["login"] = $username;
+                            $stmt = $pdo->prepare("SELECT id FROM users WHERE user_name=:uname");
+                            $stmt->bindParam(':uname', $username, PDO::PARAM_STR, 15);
+                            $stmt->execute();
+                            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                            $_SESSION["user_id"] = (int) $row["id"];
+                            header("location: ../index");
+                        }
+                        catch (PDOException $e)
+                        {
+                            login_error(1, $e->getMessage());
+                        }
+                    }
+                }
+            }
+
 		}
 		if ($_POST["submit"] == "Login")
 		{
