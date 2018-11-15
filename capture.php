8ca9f21818e7e1e72aabc3ce07c1342efedd60c9
Fred-Dee <fred.dilapisho@gmail.com>
2018-11-15 13:27:40 +0200

    mail function next

--- capture.php
+++ capture.php
@@ -21,34 +21,10 @@ if ($_SESSION["login"] == "guest") {
 
         <div class="container-fluid">
 
-            <div class="row">
-                <div class="col-xs-12">
-                    <?php
-                    require_once("./includes/Element.php");
-                    $div = new Element("div", false);
-                    $msg;
-                    $class;
-                    if (isset($_SESSION["errors"])) {
-                        $msg = $_SESSION["errors"]["errmsg"];
-                        $class = "alert alert-danger";
-                    }
-                    if (isset($_SESSION["success"])) {
-                        $msg = $_SESSION["success"]["message"];
-                        $class = "alert alert-success";
-                    }
-                    if (isset($_SESSION["errors"]) || isset($_SESSION["success"])) {
-                        $div->add_class($class);
-                        $div->add_text($msg);
-                        echo $div;
-                        unset($_SESSION["errors"]);
-                        unset($_SESSION["success"]);
-                    }
-                    ?>
-                </div>
-            </div>
+			<?php output_returns(); ?>
             <div class="row">
                 <div class="col-sm-12 col-sm-offset-12 col-md-3">
-                    <div class="flex-column" id="col-right">
+                    <div class="flex-col" id="col-right">
                     </div>
                     <canvas id="canvasVid" name="background" style="display:none;" width="500px" height="375px"></canvas>
                     <canvas id='canvasOver'name="overlay" style="display:none;" width="500px" height="375px"></canvas>
@@ -77,36 +53,36 @@ if ($_SESSION["login"] == "guest") {
                     <button class="btn btn-primary grey darken-4" onclick="uploadSnaps()" id="btn_upload">Upload Snaps</button>
                     <button class="btn btn-primary grey darken-4" onclick="clearSnaps()">Clear Snaps</button>
                 </div>
-                <div class="col-sm-12 col-md-3 flex-column">
-                    <div class="">
+                <div class="col-sm-12 col-md-3 flex-col">
+                    
                         <form class="form-overlays">
-                            <div class="form-check flex-col-item">
+                            <div class="form-check-inline flex-col-item">
                                 <input type="checkbox" class="form-check-input" id="ov_c1" />
                                 <label class="form-check-label" for="ov_c1">
                                     <img class="img-responsive " id="ov_1" src="./imgs/overlay.png" alt="" onclick=""/>
 
                                 </label>
                             </div>
-                            <div class="form-check flex-col-item">
+                            <div class="form-check-inline flex-col-item">
                                 <input type="checkbox" class="form-check-input" id="ov_c2" />
                                 <label class="form-check-label" for ="ov_c2">
                                     <img class="img-responsive" id="ov_2" src="./imgs/overlay2.png" alt="" onclick=""/>
                                 </label>
                             </div>
-                            <div class="form-check flex-col-item">
+                            <div class="form-check-inline flex-col-item">
                                 <input type="checkbox" class="form-check-input" id="ov_c3" />
                                 <label class="form-check-label" for ="ov_c2">
                                     <img class="img-responsive" id="ov_3" src="./imgs/2-2-sunglasses-picture-thumb.png" alt="" onclick=""/>
                                 </label>
                             </div>
-                            <div class="form-check flex-col-item">
+                            <div class="form-check-inline flex-col-item">
                                 <input type="checkbox" class="form-check-input" id="ov_c4" />
                                 <label class="form-check-label" for ="ov_c2">
                                     <img class="img-responsive" id="ov_4" src="./imgs/lips.png" alt="" onclick=""/>
                                 </label>
                             </div>
                         </form>
-                    </div>
+            
                 </div>
             </div>
 
