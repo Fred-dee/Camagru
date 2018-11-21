ef2cb4dd8758a839cf0c6fefe866671394e58540
Fred-Dee <fred.dilapisho@gmail.com>
2018-11-21 10:17:49 +0200

    Mail function works, just need to send from a mailinator account

--- includes/comment.php
+++ includes/comment.php
@@ -27,44 +27,19 @@ if (isset($_SESSION["login"]) && $_SESSION["login"] != "guest" && isset($_POST))
         $stmt->bindParam(":iid", $img_id, PDO::PARAM_INT, 11);
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        //var_dump($row);
         $stmt = $pdo->prepare("SELECT * FROM users where id = :uid");
         $stmt->bindParam(':uid', $row["user_id"], PDO::PARAM_INT, 11);
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         if ($row["em_subs"] == 1) {
-            $msg = 'New Password is: ' . "j5444302@nwytg.net";
-
-            $msg = wordwrap($msg, 70);
-
-            $header = "From: no-reply@pictostellar.co.za";
-
-            $bool = mail("j5444302@nwytg.net", "New Email Address", $msg, $header);
-            /*$to_mail = $row["email"];
+            $to_mail = $row["email"];
             $subject = "You have unread Comments";
-            $message = "User " . $_SESSION["login"] . ", has commented on your image saying:" . PHP_EOL . $msg . PHP_EOL . "Best\n Camagru Team";
+            $message = "User " . $_SESSION["login"] . ", has commented on your image saying:" . PHP_EOL . $msg . PHP_EOL . "Best\nCamagru Team";
             $message = str_replace("\n.", "\n..", $message);
             $headers = 'FROM: fred.dilapisho@gmail.com';
 
-            $myMail = new Mailer();
-
-            $arr_var = array(
-                "to" => $row["email"],
-                "subject" => "You have unread Comments",
-                "message" => $message,
-                "from" => "noreply@camagru.com",
-                "isSMTP" => true,
-                "username" => "fred.dilapisho@gmail.com",
-                "password" => "Dilapisho#15",
-                "smtp_port" => 587,
-                "server" => "smtp.gmail.com"
-            );
-            //$myMail->set_variables($arr_var);
             $bool = mail($to_mail, $subject, $message, $headers);
-            //$bool = $myMail->send();
-            //$mail->get_setting("smtp_server");
-             * 
-             */
+
             if ($bool == false)
                 index_error(-1, "Was unable to send a notifcation " . $row["email"]);
             else
